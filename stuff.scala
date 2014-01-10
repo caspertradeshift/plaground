19f3ae7b462f1f829ed488252f7a356a1577fc3b
Casper Rafn-Nørregård <crn@tradeshift.com>
2014-01-10 12:13:43 +0100

    updated

--- stuff.scala
+++ stuff.scala
@@ -18,7 +18,7 @@ trait BackendServiceComponent {
   val backendService: BackendService
 
   class BackendService(serviceConfig: Configuration.Service) extends ExternalService(serviceConfig, config, system, IO(Http)(system)) with IDFormats {
-    override def prefix = super.prefix + "/rest/external"
+    override def prefix = super.prefix + "/rest"
     
     lazy val proxyServiceComponent = BackendServiceComponent.this
     
